6a06ed5d33e56aacada93fa622d0adac6c182ae9
bramandityo <freez.meinster@gmail.com>
2018-05-30 13:44:56 +0700

    sudah bisa create

--- db.go
+++ db.go
@@ -5,8 +5,16 @@ import (
 
 	as "github.com/aerospike/aerospike-client-go"
 	"github.com/gin-gonic/gin"
+	uuid "github.com/satori/go.uuid"
 )
 
+// Buku test
+type Buku struct {
+	Title string `json:"title"`
+	Price uint32 `json:"price"`
+}
+
+// SetClient Middleware untuk menambahkan client
 func SetClient() gin.HandlerFunc {
 	client := GetClient()
 
@@ -16,6 +24,7 @@ func SetClient() gin.HandlerFunc {
 	}
 }
 
+// GetClient fungsi untuk membuat client baru
 func GetClient() *as.Client {
 	client, err := as.NewClient("127.0.0.1", 3000)
 	if err != nil {
@@ -30,6 +39,7 @@ func panicOnError(err error) {
 	}
 }
 
+// GetByKey digunakan untuk melakukan query ke aerospike berdasarkan key
 func GetByKey(c *gin.Context, id string) *as.Record {
 	key, err := as.NewKey("test", "buku", id)
 	readPolicy := as.NewPolicy()
@@ -37,4 +47,21 @@ func GetByKey(c *gin.Context, id string) *as.Record {
 	rec, err := client.Get(readPolicy, key)
 	panicOnError(err)
 	return rec
+}
+
+// CreateData digunakan untuk membuat data ke dalam aerospike
+func CreateData(c *gin.Context) string {
+	id := uuid.Must(uuid.NewV4()).String()
+	key, err := as.NewKey("test", "buku", id)
+	client := c.MustGet("client").(as.Client)
+	writePolicy := as.NewWritePolicy(0, 0)
+	var buku Buku
+	c.BindJSON(&buku)
+	bin := as.BinMap{
+		"title": buku.Title,
+		"price": buku.Price,
+	}
+	err = client.Put(writePolicy, key, bin)
+	panicOnError(err)
+	return id
 }
--- main.go
+++ main.go
@@ -12,13 +12,19 @@ func rootHandler(c *gin.Context) {
 func detailHandler(c *gin.Context) {
 	key := c.Param("key")
 	rec := GetByKey(c, key)
-	c.JSON(200, rec)
+	c.JSON(200, rec.Bins)
+}
+
+func createHandler(c *gin.Context) {
+	id := CreateData(c)
+	c.JSON(200, gin.H{"document_id": id})
 }
 
 func main() {
 	r := gin.New()
 	r.Use(SetClient())
 	r.GET("/", rootHandler)
+	r.POST("/", createHandler)
 	r.GET("/:key", detailHandler)
 	r.Run(":8000")
 }
